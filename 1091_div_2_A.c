b338b216d27afd00eff967065a6fbc7307f33e18
Estiak Ahmed Sonet <estiakahmedsonet@gmail.com>
2026-04-08 17:18:46 +0600

    Add files via upload
    Codeforcess Round  1091 division 2 Contest A problem Solved

---
+++ 1091_div_2_A.c
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int n,k;
+        cin>>n>>k;
+
+        int arr[n];
+
+        for(int i=0;i<n;i++){
+
+            cin>>arr[i];
+
+            if(a[i]==a[i]) {
+                cout<<"YES"<<endl;
+                break;
+            }
+            else{
+                cout<<"NO"<<endl;
+                break;
+            }
+        }
+    }
+
+
+    return 0;
+}
